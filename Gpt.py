dab2231994768189329e3afaf64867e682de46b0
IshiharaRioto <21aj013@ms.dendai.ac.jp>
2024-03-05 17:52:12 +0900

    Gpt.pyの変更を追加

--- Gpt.py
+++ Gpt.py
@@ -1,29 +1,19 @@
-from fastapi import FastAPI
 from openai import OpenAI
-import os
 
-app = FastAPI()
-client = OpenAI(
-    organization=os.environ["OPENAI_ORGANIZATION"]
+clien = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a chatbot"
+        },
+        {
+            "role": "user",
+            "content": "こんにちは"
+        }
+    ]
 )
-OpenAI.api_key = os.environ["OPENAI_API_KEY"]
 
-@app.get("/greet")
-def greet():
-    completion = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a helpful assistant."
-            },
-            {
-                "role": "user",
-                "content": "こんにちは"
-            }
-        ]
-    )
-    return {"message": completion.choices[0].message["content"]}
-
-if __name__ == '__main__':
-    print(greet())
+print(completion.choices[0].message)
