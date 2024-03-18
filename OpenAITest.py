fdb3d199b2fa86c48e4515bf4439d93733cbb72f
IshiharaRioto <21aj013@ms.dendai.ac.jp>
2024-03-18 18:54:53 +0900

    OpenAIのAPIキーと組織を環境変数から取得するように変更

--- OpenAITest.py
+++ OpenAITest.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import os
 
 OpenAI.api_key = os.environ["OPENAI_API_KEY"]
+OpenAI.organization = os.environ["OPENAI_ORGANIZATION"]
 ai = OpenAI()
 
 completion = ai.chat.completions.create(
